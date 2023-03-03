6dfc8a8be644cc12af72c66726e39798e47feaec
Aditya Saravana <aditya.saravana@icloud.com>
2023-03-02 18:39:01 -0800

    trying to fix xcode

--- Wingman/Wingman/WingmanApp.swift
+++ Wingman/Wingman/WingmanApp.swift
@@ -1,15 +1,8 @@
-/// It looks like you haven't entered a license key yet.
-/// To add your license key, follow the guide here:
-/// You can purchase a license key here: https://thedevdudedownloads.gumroad.com/l/wingman
-
-/// If this isn't your first time seeing this message, your license key is invalid.
-
-/// If this isn't your first time seeing this message, your license key is invalid.
 //
 //  WingmanApp.swift
 //  Wingman
 //
-//  Created by Aditya Saravana on 1/31/23.
+//  Created by Aditya Saravana on 3/2/23.
 //
 
 import SwiftUI
@@ -18,9 +11,7 @@ import SwiftUI
 struct WingmanApp: App {
     var body: some Scene {
         WindowGroup {
-            InstallationInstructionView()
+            ContentView()
         }
     }
 }
-
-
